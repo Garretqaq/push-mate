f3dc86cf73810b165b7e19fb540ae8d733756d70
songguangzhi <952430164@qq.com>
2023-05-12 16:21:17 +0800

    fix: 增加配置

--- push-app/src/main/java/com/dato/push/app/config/SecurityConfig.java
+++ push-app/src/main/java/com/dato/push/app/config/SecurityConfig.java
@@ -1,74 +1,82 @@
-//package com.dato.push.app.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.AuthenticationEntryPoint;
-//import org.springframework.security.web.access.AccessDeniedHandler;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//@EnableGlobalMethodSecurity(prePostEnabled =true) //开启授权注解功能
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Resource
-//    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
-//
-//    @Resource
-//    private AuthenticationEntryPoint authenticationEntryPoint;
-//
-//
-//    @Resource
-//    private AccessDeniedHandler accessDeniedHandler;
-//
-//    //放行
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                //关闭csrf
-//                .csrf().disable()
-//                //不通过Session获取SecurityContext
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                // 对于登录接口 允许匿名访问
-//                .antMatchers("/user/login").anonymous()
-//                .antMatchers("/testCors").hasAuthority("system:dept:list211")
-//                // 除上面外的所有请求全部需要鉴权认证
-//                .anyRequest().authenticated();
-//
-//        //将jwtAuthenticationTokenFilter过滤器放到登录认证之前
-//        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//
-//        //配置异常处理器
-//        http.exceptionHandling()
-//                //认证失败处理器
-//                .authenticationEntryPoint(authenticationEntryPoint)
-//                .accessDeniedHandler(accessDeniedHandler);
-//
-//
-//        //允许跨域
-//        http.cors();
-//    }
-//
-//    @Bean  //这样子就可以从容器当中获取到AuthenticationManager
-//    @Override
-//    protected AuthenticationManager authenticationManager() throws Exception {
-//        return super.authenticationManager();
-//    }
-//}
+package com.dato.push.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled =true) //开启授权注解功能
+public class SecurityConfig {
+    @Resource
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
+    @Resource
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Resource
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                //关闭csrf
+                .csrf().disable()
+                //不通过Session获取SecurityContext
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // 对于登录接口 允许匿名访问
+                .antMatchers("/user/login").anonymous()
+                .antMatchers("/testCors").hasAuthority("system:dept:list211")
+                // 除上面外的所有请求全部需要鉴权认证
+                .anyRequest().authenticated();
+
+        //将jwtAuthenticationTokenFilter过滤器放到登录认证之前
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        //配置异常处理器
+        http.exceptionHandling()
+                //认证失败处理器
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler);
+
+        //允许跨域
+        http.cors();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(new UserDetailsService() {
+                    @Override
+                    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                        return null;
+                    }
+                })
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+}
---
+++ push-app/src/main/java/com/dato/push/app/service/LoginService.java
@@ -0,0 +1,11 @@
+package com.dato.push.app.service;
+
+/**
+ * 登录接口
+ * @since 2023-05-12
+ * @author sgz
+ */
+public class LoginService {
+
+
+}
