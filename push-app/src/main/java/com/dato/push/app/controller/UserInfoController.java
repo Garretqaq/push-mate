be5846ae414f204ee8f8e2f911c136b2c42e806b
‘憨憨大头’ <952430164@qq.com>
2023-05-14 20:35:50 +0800

    fix：增加用户权限

---
+++ push-app/src/main/java/com/dato/push/app/controller/UserInfoController.java
@@ -0,0 +1,14 @@
+package com.dato.push.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户信息 控制器
+ */
+@RestController
+@RequestMapping("/user/info")
+public class UserInfoController {
+
+
+}
---
+++ push-app/src/main/java/com/dato/push/app/model/rep/UserAuthorityResponse.java
@@ -0,0 +1,26 @@
+package com.dato.push.app.model.rep;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 用户权限返回对象
+ *
+ * @since 2023/05/14
+ * @author sgz
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserAuthorityResponse {
+    /**
+     * 用户名
+     */
+    private String name;
+    /**
+     * 可以访问路径
+     */
+    private String url;
+
+}
